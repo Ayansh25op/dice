cffeddcf7cdb701c48dcb8b3f527eb9c1a413d43
Ayansh25op <87590978+Ayansh25op@users.noreply.github.com>
2021-09-29 16:40:26 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -56,3 +56,60 @@ input.onGesture(Gesture.Shake, function () {
             `)
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Number2 = randint(1, 6)
+    if (Number2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (Number2 == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (Number2 == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (Number2 == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (Number2 == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (Number2 == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
